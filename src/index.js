9415f950dca12cd9f3eeaf3206e80323a0c00402
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2024-12-09 19:50:24 +0100

    list of steps in index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./style.css"
+// logic list containing project
+// logic create project
+// logic add new project
+// logic delete project
+
+// logic create list task
+// logic add task to list project
+// logic remove task from list project
+// logic toggle completed task
+// change todo priority
+
+//localstorage the data
+
+
