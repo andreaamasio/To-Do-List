cb85c5a574350ad9ab2c8e6cd2e38b66de844005
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2024-12-11 15:02:15 +0100

    created DOM manipulation

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+const newProjectButton=document.querySelector("#new-project-button")
+
+function askProjectName(){
+    let projectName=prompt("How do you call this new project list?", "New Project")
+    return projectName
+}
