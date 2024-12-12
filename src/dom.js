6af0e86eb3e1da452d58083c371e7159e0543802
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2024-12-12 18:33:34 +0100

    Feat add new task form style to connect logic to UI and dom creation

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,18 @@
 const newProjectButton=document.querySelector("#new-project-button")
-
+const content=document.querySelector('#content')
 function askProjectName(){
     let projectName=prompt("How do you call this new project list?", "New Project")
     return projectName
+}
+function createDOMProject(){
+    // project header
+    const projectHeader=document.createElement('div')
+
+    const addTaskButton=document.createElement('button')
+    addTaskButton.setAttribute('class','add-task')
+    addTaskButton.textContent='Add Task'
+    addTaskButton.addEventListener('click', AddTask)
+    projectHeader.appendChild(addTaskButton)
+
+    
 }
